1126f69114dcbcf4c97f943e230a3dabd0298c9e
Wulf <cosminrosca1995@gmail.com>
2018-05-24 21:54:58 +0300

    New Version

---
+++ pfUI-CustomMedia-v1.1/pfUI-CustomMedia.lua
@@ -0,0 +1,30 @@
+
+--- Custom Unitframe Status bar Textures ---
+
+table.insert(pfUI.gui.dropdowns.uf_bartexture, "Interface\\AddOns\\pfUI-CustomMedia-v1.1\\bars\\pfUI-A.tga:pfUI-A")
+table.insert(pfUI.gui.dropdowns.uf_bartexture, "Interface\\AddOns\\pfUI-CustomMedia-v1.1\\bars\\pfUI-B.tga:pfUI-B")
+table.insert(pfUI.gui.dropdowns.uf_bartexture, "Interface\\AddOns\\pfUI-CustomMedia-v1.1\\bars\\pfUI-C.tga:pfUI-C")
+table.insert(pfUI.gui.dropdowns.uf_bartexture, "Interface\\AddOns\\pfUI-CustomMedia-v1.1\\bars\\pfUI-D.tga:pfUI-D")
+table.insert(pfUI.gui.dropdowns.uf_bartexture, "Interface\\AddOns\\pfUI-CustomMedia-v1.1\\bars\\pfUI-E.tga:pfUI-E")
+table.insert(pfUI.gui.dropdowns.uf_bartexture, "Interface\\AddOns\\pfUI-CustomMedia-v1.1\\bars\\pfUI-F.tga:pfUI-F")
+table.insert(pfUI.gui.dropdowns.uf_bartexture, "Interface\\AddOns\\pfUI-CustomMedia-v1.1\\bars\\pfUI-G.tga:pfUI-G")
+table.insert(pfUI.gui.dropdowns.uf_bartexture, "Interface\\AddOns\\pfUI-CustomMedia-v1.1\\bars\\pfUI-H.tga:pfUI-H")
+table.insert(pfUI.gui.dropdowns.uf_bartexture, "Interface\\AddOns\\pfUI-CustomMedia-v1.1\\bars\\pfUI-I.tga:pfUI-I")
+table.insert(pfUI.gui.dropdowns.uf_bartexture, "Interface\\AddOns\\pfUI-CustomMedia-v1.1\\bars\\pfUI-J.tga:pfUI-J")
+table.insert(pfUI.gui.dropdowns.uf_bartexture, "Interface\\AddOns\\pfUI-CustomMedia-v1.1\\bars\\pfUI-K.tga:pfUI-K")
+table.insert(pfUI.gui.dropdowns.uf_bartexture, "Interface\\AddOns\\pfUI-CustomMedia-v1.1\\bars\\pfUI-L.tga:pfUI-L")
+table.insert(pfUI.gui.dropdowns.uf_bartexture, "Interface\\AddOns\\pfUI-CustomMedia-v1.1\\bars\\pfUI-M.tga:pfUI-M")
+table.insert(pfUI.gui.dropdowns.uf_bartexture, "Interface\\AddOns\\pfUI-CustomMedia-v1.1\\bars\\pfUI-N.tga:pfUI-N")
+table.insert(pfUI.gui.dropdowns.uf_bartexture, "Interface\\AddOns\\pfUI-CustomMedia-v1.1\\bars\\pfUI-O.tga:pfUI-O")
+table.insert(pfUI.gui.dropdowns.uf_bartexture, "Interface\\AddOns\\pfUI-CustomMedia-v1.1\\bars\\pfUI-P.tga:pfUI-P")
+
+--- Custom Fonts ---
+
+table.insert(pfUI.gui.dropdowns.fonts, "Interface\\AddOns\\pfUI-CustomMedia-v1.1\\fonts\\pfUI-A.ttf:pfUI-A")
+table.insert(pfUI.gui.dropdowns.fonts, "Interface\\AddOns\\pfUI-CustomMedia-v1.1\\fonts\\pfUI-B.ttf:pfUI-B")
+table.insert(pfUI.gui.dropdowns.fonts, "Interface\\AddOns\\pfUI-CustomMedia-v1.1\\fonts\\pfUI-C.ttf:pfUI-C")
+table.insert(pfUI.gui.dropdowns.fonts, "Interface\\AddOns\\pfUI-CustomMedia-v1.1\\fonts\\pfUI-D.ttf:pfUI-D")
+table.insert(pfUI.gui.dropdowns.fonts, "Interface\\AddOns\\pfUI-CustomMedia-v1.1\\fonts\\pfUI-E.ttf:pfUI-E")
+table.insert(pfUI.gui.dropdowns.fonts, "Interface\\AddOns\\pfUI-CustomMedia-v1.1\\fonts\\pfUI-F.ttf:pfUI-F")
+table.insert(pfUI.gui.dropdowns.fonts, "Interface\\AddOns\\pfUI-CustomMedia-v1.1\\fonts\\pfUI-G.ttf:pfUI-G")
+table.insert(pfUI.gui.dropdowns.fonts, "Interface\\AddOns\\pfUI-CustomMedia-v1.1\\fonts\\pfUI-G.ttf:pfUI-H")
